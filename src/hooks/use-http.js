181fe667e5bf0e05f9aa0081db77ec83de3710b1
Cameron Miner <cameronsminer@gmail.com>
2022-12-21 11:42:04 -0700

    Added custom hook

---
+++ src/hooks/use-http.js
@@ -0,0 +1,37 @@
+import { useState, useCallback } from "react";
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      applyData(data); // Data will be sent back to component, where that component has a specific function to handle that data in the way it needs
+
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading: isLoading,
+    error: error,
+    sendRequest: sendRequest,
+  };
+};
+
+export default useHttp;
